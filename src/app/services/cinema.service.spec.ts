838e7f16fb06af23583bca897b41188b76ad3d32
Ayoub <ayoubmouallim@gmail.com>
2021-06-07 14:37:42 +0100

    first commit

---
+++ src/app/services/cinema.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { CinemaService } from './cinema.service';
+
+describe('CinemaService', () => {
+  let service: CinemaService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CinemaService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
